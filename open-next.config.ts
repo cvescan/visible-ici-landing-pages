a3bb81668792e2b1ffb087df87be998141131c4c
Cyril <cyril.vescan@gmail.com>
2025-10-30 16:26:15 +0100

    chore: prep nextjs project for cloudflare deploy

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,4 +1,11 @@
-export default {
-  edge: true,
-  compatibilityDate: '2024-04-01'
+import type { OpenNextConfig } from '@opennextjs/cloudflare';
+import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+
+const baseConfig = defineCloudflareConfig();
+
+const config: OpenNextConfig = {
+  ...baseConfig,
+  // Extend here if we need to tweak the default Cloudflare adapter behavior.
 };
+
+export default config;
